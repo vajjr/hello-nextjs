b4dbfbe7081c1b970c9c9d8987e72172b2be7589
Valdir Junior <vajjr@valdirjunior.com.br>
2020-12-23 21:44:35 -0300

    getStaticProps

---
+++ pages/tempo.js
@@ -0,0 +1,30 @@
+function Tempo(props) {
+
+    const dynamicDate = new Date();
+    const dynamicDateString = dynamicDate.toGMTString();
+
+    return (
+        <div>
+            <h1>Diâmico</h1>
+            <p>
+                {dynamicDateString} (dinâmico)
+            </p>
+            <p>
+                {props.staticDateString} (estático)
+            </p>
+        </div>
+    );
+}
+
+export function getStaticProps() {
+    const staticDate = new Date();
+    const staticDateString = staticDate.toGMTString();
+
+    return {
+        props: {
+            staticDateString
+        }
+    }
+}
+
+export default Tempo;
